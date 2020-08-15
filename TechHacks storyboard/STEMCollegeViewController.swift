8409747c3b2cfe553432e140e4c162f4c895fc91
Rebecca DeBenedictis <rjdebened@verizon.net>
2020-08-15 18:08:40 -0400

    rebecca 6:00 pm

--- TechHacks storyboard/STEMCollegeViewController.swift
+++ TechHacks storyboard/STEMCollegeViewController.swift
@@ -9,13 +9,47 @@
 import UIKit
 
 class STEMCollegeViewController: UIViewController {
-
+    @IBOutlet weak var school: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func compsci(_ sender: UIButton) {
+        goodschools (choice: 0)
+    }
+    
+    @IBAction func bio(_ sender: UIButton) {
+        goodschools (choice: 1)
+    }
+    
+    @IBAction func biostats(_ sender: UIButton) {
+        goodschools (choice: 2)
+    }
+    
+    @IBAction func AI(_ sender: UIButton) {
+        goodschools (choice: 3)
+    }
+    @IBAction func cstheory(_ sender: UIButton) {
+        goodschools (choice: 4)
+    }
+    
+    func goodschools (choice : Int) {
+            if choice == 0 {
+            school.text = "UC Berkeley (8.5% acceptance rate), Carnegie Mellon (5% for cs), MIT (7%)"
+            } else if choice == 1 {
+           school.text = "Johns Hopkins (11.5%), California Institute of Technology (7%), Stanford (4.4%)"
+            } else if choice == 2 {
+              school.text = "University of Washington (48%), UNC Chapel Hill (22.2%), University of Chicago (7.3%)"
+            } else if choice == 3 {
+                   school.text = "University of Washington (48%), UNC Chapel Hill (22.2%), University of Chicago (7.3%)"
+               
+            } else if choice == 4 {
+                 school.text = "MIT (6.7%), Cornell (10.6%), UCLA(8% cs)"
+        }
+    }
 
     /*
     // MARK: - Navigation
--- TechHacks storyboard/STEMHighViewController.swift
+++ TechHacks storyboard/STEMHighViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class STEMHighViewController: UIViewController {
-
+    @IBOutlet weak var courseinfo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func compsciprin(_ sender: UIButton) {
+        apclass(choice: 0)
+    }
+    @IBAction func compscia(_ sender: UIButton) {
+        apclass(choice: 1)
+    }
+    @IBAction func chem(_ sender: UIButton) {
+        apclass(choice: 2)
+    }
+    @IBAction func enviornmental(_ sender: UIButton) {        apclass(choice: 3)
+    }
+    @IBAction func bio(_ sender: UIButton) {        apclass(choice: 4)
+    }
+    func apclass (choice : Int) {
+            if choice == 0 {
+            courseinfo.text = "Introduces you to the breadth of the field of computer science.Learn how to design and evaluate solutions and to apply computer science to solve problems through the development of algorithms and programs."
+            } else if choice == 1 {
+            courseinfo.text = "Cultivate your understanding of coding through analyzing, writing, and testing code as you explore concepts like modularity, variables, and control structures."
+            } else if choice == 2 {
+                 courseinfo.text = "Inquiry-based lab investigations. You can explore the four Big Ideas: scale, proportion, and quantity; structure and properties of substances; transformations; and energy."
+            } else if choice == 3 {
+                 courseinfo.text = "Explore and investigate the interrelationships of the natural world and analyze environmental problems, both natural and human-made. Take part in laboratory investigations and field work"
+            } else if choice == 4 {
+                 courseinfo.text = "Study inquiry-based investigations as you explore topics like evolution, energetics, information storage and transfer, and system interactions."
+        }
+    }
+     
+     
+
     
 
     /*
